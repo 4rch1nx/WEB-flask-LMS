f5394baf97aa9312050dd430e942f78f6cf4b532
SpaceLink <128892453+Andrew-24coop@users.noreply.github.com>
2025-03-13 22:33:08 +0300

    wemos8266 update

--- ESP8266/WEBflaskLMS.ino
+++ ESP8266/WEBflaskLMS.ino
@@ -1,38 +1,45 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "ESP8266_CAR";
-const char* password = "espvsflask25";
+const char* ssid = "ESP8266_AP";    // WiFi Name
+const char* password = "12345678";  // Password (min 8 chars)
 
 ESP8266WebServer server(80);
-const int ledPin = D4;
+const int ledPin = D4;  // LED pin
 bool ledState = false;
 
+// Toggle LED state (inverted logic)
 void handleToggle() {
-    ledState = !ledState;
-    digitalWrite(ledPin, ledState ? HIGH : LOW);
-    server.send(200, "text/plain", ledState ? "ON" : "OFF");
+  ledState = !ledState;
+  digitalWrite(ledPin, ledState ? LOW : HIGH);  // Inverted
+  server.send(200, "text/plain", ledState ? "ON" : "OFF");
+}
+
+// Status endpoint
+void handleStatus() {
+  server.send(200, "text/plain", "CONNECTED");
 }
 
 void setup() {
-    Serial.begin(115200);
-    
-    // Set ESP8266 as an Access Point
-    WiFi.softAP(ssid, password);
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("ESP8266 AP IP Address: ");
-    Serial.println(IP);
+  Serial.begin(115200);
 
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
+  // Set ESP8266 as an Access Point
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("ESP8266 AP IP Address: ");
+  Serial.println(IP);
 
-    // API route
-    server.on("/toggle", handleToggle);
-    
-    server.begin();
-    Serial.println("HTTP server started");
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);  // Default OFF (inverted)
+
+  // API routes
+  server.on("/toggle", handleToggle);
+  server.on("/status", handleStatus);
+
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-    server.handleClient();
+  server.handleClient();
 }
